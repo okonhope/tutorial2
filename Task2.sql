e26dcf13507396c680d8b6353e6de3999043d1a9
aliciaastro <okona4380@gmail.com>
2024-08-24 16:07:32 +0000

    loading data using copy

---
+++ Task2.sql
@@ -0,0 +1,40 @@
+CREATE  OR REPLACE DATABASE mydatabase;
+
+CREATE OR REPLACE TEMPORARY TABLE mycsvtable
+( 
+ id INTEGER,
+ last_name STRING,
+ first_name STRING,
+ company STRING,
+ email STRING,
+ workphone STRING,
+ cellphone STRING,
+ streetaddress STRING,
+ city STRING,
+ postalcode STRING
+);
+
+CREATE OR REPLACE TEMPORARY TABLE myjsontable (json_data VARIANT);
+
+CREATE OR REPLACE WAREHOUSE mywarehouse WITH
+     WAREHOUSE_SIZE = 'X-SMALL'
+     AUTO_SUSPEND = 120
+     AUTO_RESUME = TRUE
+     INITIALLY_SUSPENDED = TRUE;
+
+CREATE OR REPLACE FILE FORMAT mycsvformat
+TYPE = 'CSV'
+FIELD_DELIMITER = '|'
+SKIP_HEADER = 1;
+
+CREATE OR REPLACE FILE FORMAT myjsonformat
+TYPE = 'JSON'
+STRIP_OUTER_ARRAY = TRUE;
+
+CREATE OR REPLACE STAGE my_csv_stage
+FILE_FORMAT = mycsvformat
+URL = 's3://snowflake-doc';
+
+CREATE OR REPLACE STAGE my_json
+FILE FORMAT = myjsonformat
+URL = 's3://snowflake-docs';
